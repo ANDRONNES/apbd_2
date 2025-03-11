25c7207040ed462f3795195d83e24ece8c39d7fe
s30001 <s30001@pjwstk.edu.pl>
2025-03-11 15:54:05 +0100

    variables

---
+++ apbd_2/apbd_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num = 0;
+float  num2 = 0.8878f;
+double  num3 = 0.8878;
+bool  flag = false;
+char character = 'a';
+string s = null;
+
+
+var k = 0;
+var k1 = "text";
+
